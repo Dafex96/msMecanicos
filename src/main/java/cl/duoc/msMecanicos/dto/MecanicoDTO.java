f2de4d2800878f4b8e28f2ca488a506b8acfd94d
Dafex96 <davicolo717@gmail.com>
2026-06-23 16:55:43 -0400

    minor fix

--- src/main/java/cl/duoc/msMecanicos/dto/MecanicoDTO.java
+++ src/main/java/cl/duoc/msMecanicos/dto/MecanicoDTO.java
@@ -1,5 +1,6 @@
 package cl.duoc.msMecanicos.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "DTO que representa a un mecánico")
 public class MecanicoDTO {
 
+    @Schema(description = "ID del mecánico")
     private Integer id;
+    
+    @Schema(description = "Nombre del mecánico")
     private String nombre;
 }
